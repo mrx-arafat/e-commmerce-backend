3c971d50b753c175f8a1464a8d636c6356b4da1d
mrx-arafat <arafatmrx@gmail.com>
2024-05-22 04:26:40 +0600

    vercel deploy

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const productRoutes_1 = __importDefault(require("./Routes/productRoutes"));
+const orderRoutes_1 = __importDefault(require("./Routes/orderRoutes"));
 const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-// application routes
+// routes
+app.use('/api/products', productRoutes_1.default);
+app.use('/api/orders', orderRoutes_1.default);
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.send('Welcome to my E-commerce API');
+});
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+});
+app.use((req, res) => {
+    res.status(404).json({ success: false, message: 'Route not found' });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('hello BSDK');
+  res.send('Welcome to my E-commerce API');
 });
 
 // Error handling middleware
