4ca19255391d2e99568713bf6fc94ff653e091a2
mrx-arafat <arafatmrx@gmail.com>
2024-05-21 12:30:34 +0600

    architecture

--- src/app.js
+++ src/app.js
@@ -2,8 +2,5 @@ var express = require("express");
 var app = express();
 var port = 3000;
 app.get("/", function (req, res) {
-    res.send("Hello World!");
-});
-app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
+  res.send("Hello World!");
 });
